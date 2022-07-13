0996abecc1f21c073b44b00aa597fb2207550175
TipsyDr <tispydr@163.com>
2022-07-13 17:59:09 +0800

    feat: 分页

--- src/pages/webassembly/index.tsx
+++ src/pages/webassembly/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Button } from '@/stories/Button/Button';
 import { Pagination } from '@/stories/PageNation/pagination';
+import './index.less';
 
 const Webassembly: FC = () => {
   return (
@@ -12,15 +13,31 @@ const Webassembly: FC = () => {
         size="small"
         backgroundColor="#22a30e"
       />
-      <Pagination
-        start={0}
-        limit={10}
-        total={199}
-        maxPagerCount={5}
-        showGotoPager
-        showSelectSize
-        // onChangePager={action('onChangePager')}
-      />
+      <div>
+        <Pagination
+          start={1}
+          limit={10}
+          total={199}
+          maxPagerCount={5}
+          showGotoPager
+          showSelectSize
+          // onChangePager={action('onChangePager')}
+          classes={{
+            root: 'root',
+            ul: 'ul',
+            'prev-dot-page': 'prev-dot-page',
+            'next-dot-page': 'next-dot-page',
+            li: 'li',
+            'goto-page': 'goto-page',
+            'page-text': 'page-text',
+            'goto-page-input': 'goto-page-input',
+            'total-page-text': 'total-page-text',
+            'prev-arrow-page': 'prev-arrow-pag',
+            'next-arrow-page': 'next-arrow-page',
+            'active-page': 'active-page',
+          }}
+        />
+      </div>
     </>
   );
 };
